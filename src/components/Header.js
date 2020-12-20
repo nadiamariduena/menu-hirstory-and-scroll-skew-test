8d7aa0d1c1403fc839f06c687addecfe1d163124
melissa <nadia.mm.mariduena@gmail.com>
2020-12-20 08:54:28 +0100

    header and hamburger

---
+++ src/components/Header.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Hamburger from "./Hamburger";
+// type: sfc snippet: const  = () => {
+//
+//1
+const Header = () => {
+  //STATE FOR Menu withRouter related
+
+  //
+  //
+  //
+  //2
+  const [state, setState] = useState({
+    //   BY DEFAULT IT IS "Menu" because nothing is happening
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+    color: false,
+  });
+  //
+  //
+  //STATE FOR disabled Button
+  // 5 create a second useSate ,to prevent spam when opening and closing the menu
+  //
+  const [disabled, setDisabled] = useState(false);
+
+  //
+
+  //
+  //
+  // 3 this function is related to the event on click of the button menu, it will handle the state on change in step 2
+  const handleMenu = () => {
+    //   7
+    disabledMenu();
+    //
+    //
+    // step 3
+    if (state.initial === false) {
+      setState({
+        initial: null, //means that we already clicked at least once
+        clicked: true,
+        menuName: "Close",
+      });
+      //  -------------
+
+      console.log(1);
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+      console.log(2);
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+      console.log(3);
+    }
+  };
+  //
+  // 6 function related to step 5
+  //This function will let us know if the menu button will be disabled or not
+  //   this function will be called in step 3
+  const disabledMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 500); //we want this to run every 12 hundred milisecs
+  }; //
+  //
+  return (
+    <header>
+      <div className="inner-header">
+        <div className="logo">
+          <Link to="/">TESTING</Link>
+        </div>
+        {/* ---------- */}
+        <div className="menu">
+          <button disabled={disabled} onClick={handleMenu}>
+            {state.menuName}
+          </button>
+        </div>
+      </div>
+
+      <Hamburger state={state} />
+    </header>
+  );
+};
+
+export default Header;
+// withRouter will give access to the history PROP
+// with the history we will be able to determine if the URL has been changed, so
+// if it s change we will execute a certain command
