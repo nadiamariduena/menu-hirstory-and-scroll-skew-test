8c9cca52709d915716890bc05ff2a0fc4cbefdb4
melissa <nadia.mm.mariduena@gmail.com>
2020-12-20 09:17:17 +0100

    adding two sections to then start with the scroll skew SETUP

--- src/App.js
+++ src/App.js
@@ -59,6 +59,14 @@ function Home() {
           to new heights.
         </h5>
       </div>
+      {/* ---------- */}
+      <div className="home-content-middle">
+        <h5>
+          The <b>HAMBRG</b>, is a creative, engineer driven, global agency
+          working on advancing the software, advertising and design communities
+          to new heights.
+        </h5>
+      </div>
     </div>
   );
 }
